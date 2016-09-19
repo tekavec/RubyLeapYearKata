b4b96cffa5071742ab593dab94297b1ac3e54d0b
Tomaz Tekavec <tomaz@codurance.com>
2016-09-19 08:37:55 +0100

    Refactored tests

--- test/year_test.rb
+++ test/year_test.rb
@@ -3,14 +3,19 @@ require 'test/unit'
 
 class YearTest < Test::Unit::TestCase
   def test_leap_year_is_divisible_by_4
-    year = Year.new(1996)
-
-    assert_true(year.is_leap_year)
+    assert_true(is_leap_year(1996))
   end
 
-  def test_non_leap_year_is_indivisible_by_4
-    year = Year.new(801)
+  def test_non_leap_year_is_not_divisible_by_4
+    assert_false(is_leap_year(801))
+  end
 
-    assert_false(year.is_leap_year)
+  def test_leap_year_is_divisible_by_400
+    assert_true(is_leap_year(800))
+  end
+
+  def is_leap_year(year)
+    a_year = Year.new(year)
+    a_year.is_leap_year
   end
 end
